0b06a3878b93dbe48298b84a0dd905c1e188921d
aj3011 <ashreyjairath@gmail.com>
2023-05-13 14:41:51 -0700

    Making changes in gitignore

---
+++ build/precache-manifest.ad3034137ee1dc79baa7d1f3849906e9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afb93b1c7033bafd239c154f69812c51",
+    "url": "/CS-Hut/index.html"
+  },
+  {
+    "revision": "12061f0ec771085cd174",
+    "url": "/CS-Hut/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "df3aec14375e8ccea8a5",
+    "url": "/CS-Hut/static/css/main.070f5b1b.chunk.css"
+  },
+  {
+    "revision": "12061f0ec771085cd174",
+    "url": "/CS-Hut/static/js/2.6dc1c834.chunk.js"
+  },
+  {
+    "revision": "d0eb7e4114917ac9c9f36f5235276c94",
+    "url": "/CS-Hut/static/js/2.6dc1c834.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df3aec14375e8ccea8a5",
+    "url": "/CS-Hut/static/js/main.2c1560aa.chunk.js"
+  },
+  {
+    "revision": "31de4817701e4e553ea1",
+    "url": "/CS-Hut/static/js/runtime-main.c96bf040.js"
+  },
+  {
+    "revision": "9ae0778ee9a9ea3c052e7a0f0d4901b3",
+    "url": "/CS-Hut/static/media/bannerback.9ae0778e.mp4"
+  },
+  {
+    "revision": "38a79f6749ec1ede33cbf522a4de9040",
+    "url": "/CS-Hut/static/media/logo.38a79f67.png"
+  },
+  {
+    "revision": "465ffb2486cf55fc4ba29d6daa7543ae",
+    "url": "/CS-Hut/static/media/notfound2.465ffb24.svg"
+  }
+]);
